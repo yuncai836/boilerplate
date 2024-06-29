0c01920a65a9e98d4774d15c969adff9f852b24c
yuncai836 <167850518+yuncai836@users.noreply.github.com>
2024-06-29 12:57:21 +0800

    优化 OpenTelemetry 错误处理
    在 otel 初始化过程中增加了错误处理函数。在原有的基础上，此更新将 OpenTelemetry 的内部错误通过 zap logger 输出，可提升调试效率。同时添加了新的错误处理函数类型，并实现了 Handle 方法以满足 otel 的错误处理接口。

--- otel/init.go
+++ otel/init.go
@@ -3,10 +3,14 @@ package otel
 import (
 	"context"
 	"github.com/spf13/viper"
+	"go.opentelemetry.io/otel"
 	"go.uber.org/zap"
 )
 
 func Init(ctx context.Context, logger *zap.Logger, serviceName, grpcEndpoint, httpEndpoint string) func() {
+	otel.SetErrorHandler((eh)(func(err error) {
+		logger.Error("otel internal error", zap.Error(err))
+	}))
 	shutdownTracer := InitTracer(ctx, logger, serviceName, grpcEndpoint)
 	shutdownLog := InitLog(ctx, logger, serviceName, httpEndpoint)
 	shutdownMeter := InitMeter(ctx, logger, serviceName, grpcEndpoint)
@@ -25,4 +29,10 @@ func InitFromViper(ctx context.Context, logger *zap.Logger, viper *viper.Viper) 
 		viper.GetString("otel.endpoints.grpc"),
 		viper.GetString("otel.endpoints.http"),
 	)
+}
+
+type eh func(err error)
+
+func (e eh) Handle(err error) {
+	e(err)
 }
