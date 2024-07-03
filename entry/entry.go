3c76951c21755089cd4e059f231119d9cb6c1221
yuncai836 <167850518+yuncai836@users.noreply.github.com>
2024-07-03 16:40:44 +0800

    添加上下文支持到 GracefulServe 函数
    该提交主要在 GracefulServe 函数中添加了上下文 (context) 支持，并修改了函数参数，将原本由 spawner 函数调用的上下文改为 GracefulServe 函数的入口参数。这样做可以更好地跟踪函数的生命周期，例如，我们可以在函数开始时启动跟踪，并在任务失败或成功时结束跟踪。这对于改进故障排除和监视性能非常有用。

--- entry/entry.go
+++ entry/entry.go
@@ -1,29 +1,40 @@
 package entry
 
 import (
+	"context"
 	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"github.com/yuncai836/boilerplates/otel"
 	"go.uber.org/zap"
 	"os"
 	"os/signal"
 )
 
-func GracefulServe[T any](c *T, cs chan *T, spawner func(c *T) (func(), error)) {
+func GracefulServe[T any](c *T, cs chan *T, spawner func(ctx context.Context, c *T) (func(), error)) {
 	ss := make(chan os.Signal, 1)
 	defer close(ss)
 	signal.Notify(ss, os.Interrupt)
-	shutdown, err := spawner(c)
+	var ctx = context.Background()
+	ctx, span := otel.Tracer.Start(ctx, "graceful server start")
+	shutdown, err := spawner(ctx, c)
 	if err != nil {
-		otelzap.L().Error("spawn serve task fail", zap.Error(err))
+		otelzap.L().ErrorContext(ctx, "spawn serve task fail", zap.Error(err))
+		span.End()
 		return
 	}
+	span.End()
 	for {
 		select {
 		case c = <-cs:
+			ctx, span := otel.Tracer.Start(ctx, "graceful server reload")
 			shutdown()
-			shutdown, err = spawner(c)
+			shutdown, err = spawner(ctx, c)
 			if err != nil {
-				otelzap.L().Error("reload server fail", zap.Error(err), zap.Any("config", c))
+				otelzap.L().ErrorContext(ctx, "reload server fail", zap.Error(err), zap.Any("config", c))
+				span.End()
+				continue
 			}
+			otelzap.L().InfoContext(ctx, "reload server success")
+			span.End()
 		case s := <-ss:
 			otelzap.L().Info("receive os signal", zap.String("signal", s.String()))
 			shutdown()
